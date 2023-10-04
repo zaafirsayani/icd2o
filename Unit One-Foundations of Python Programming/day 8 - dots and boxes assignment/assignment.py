19baac0058bb4a2e303855ce65361151fce07a33
Zaafir Sayani <zsayani@bayviewglen.ca>
2023-10-04 13:54:25 -0400

    progress from assignment

---
+++ Unit One-Foundations of Python Programming/day 8 - dots and boxes assignment/assignment.py
@@ -0,0 +1,48 @@
+game1opponentname = input("Enter your opponents name from game 1: ")
+game1points = input("Enter your amount of points from game 1: ")
+game1_opponentpoints = input("Enter your opponents points from game 1: ")
+
+game2opponentname = input("Enter your opponents name from game 2: ")
+game2points = input("Enter your amount of points from game 2: ")
+game2_opponentpoints = input("Enter your opponents points from game 2: ")
+
+game3opponentname = input("Enter your opponents name from game 3: ")
+game3points = input("Enter your amount of points from game 3: ")
+game3_opponentpoints = input("Enter your opponents points from game 3: ")
+
+game4opponentname = input("Enter your opponents name from game 4: ")
+game4points = input("Enter your amount of points from game 4: ")
+game4_opponentpoints = input("Enter your opponents points from game 4")
+
+game5opponentname = input("Enter your opponents name from game 5: ")
+game5points = input("Enter your amount of points from game 5: ")
+game5_opponentpoints = input("Enter your opponents points from game 5: ")
+
+print(game1opponentname)
+print(game1points)
+print(game1_opponentpoints)
+
+print(game2opponentname)
+print(game2points)
+print(game2_opponentpoints)
+
+print(game3opponentname)
+print(game3points)
+print(game3_opponentpoints)
+
+print(game4opponentname)
+print(game4points)
+print(game4_opponentpoints)
+
+print(game5opponentname)
+print(game5points)
+print(game5_opponentpoints)
+
+print(input("Enter your name: "))
+print()
+print("Game 1: ")
+print(game1opponentname)
+
+
+
+
